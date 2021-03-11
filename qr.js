cd635d40eb50962fc34d9ba57e02c6a3229e4163
mehultilala <mehultilala1993@gmail.com>
2021-03-11 18:02:25 +0530

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,5 @@
+ /*device look https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_device_smartphone*/
+/*https://www.w3schools.com/w3css/tryw3css_templates_band.htm
+*/
+/* parralax*/ 
+/*https://www.w3schools.com/howto/howto_css_cutout_text.asp*/
